6510d35947a2dac8ca27ea73d26874dfb89b4b51
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-18 19:01:20 -0400

    created list of token names

--- lex.py
+++ lex.py
@@ -1,3 +1,23 @@
 import ply.lex as lex
 
 # Required list of token names
+tokens = (
+    'NAME',
+    'REBALANCE',
+    'TICKER',
+    'NUMBER',
+    'GTE',
+    'LTE',
+    'GT',
+    'LT',
+    'INDICATOR',
+    'IF',
+    'ELSE',
+    'FILTER',
+    'ASSET',
+    'LPAREN',
+    'RPAREN',
+    'LBRACKET',
+    'RBRACKET',
+    'QUOTATION',
+)

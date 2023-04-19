a2d09c71819aec42fcf226fb3c82e0a2b6e3b9cb
slicerrr0 <mrslicerr0@protonmail.com>
2023-04-19 16:35:38 -0400

    removed SPACE token from grammar rules for reserved words

--- symphyacc.py
+++ symphyacc.py
@@ -16,42 +16,42 @@ def p_group_expr(p):
 
 # Identifies 'weight specified' terminals
 def p_weight_specified_expr(p):
-    'weight_specified : LBRACKET LPAREN WEIGHT_SPECIFIED SPACE'
+    'weight_specified : LBRACKET LPAREN WEIGHT_SPECIFIED'
     p[0] = p[3]
 
 # Identifies 'weight equal' terminals
 def p_weight_equal_expr(p):
-    'weight_equal : LBRACKET LPAREN WEIGHT_EQUAL SPACE'
+    'weight_equal : LBRACKET LPAREN WEIGHT_EQUAL'
     p[0] = p[3]
 
 # Identifies 'rebalance' terminals
 def p_rebal_expr(p):
-    'rebal : LBRACE COLON REBALANCE SPACE'
+    'rebal : LBRACE COLON REBALANCE'
     p[0] = p[3]
 
 # Identifies 'filter' terminals
 def p_filter_expr(p):
-    'filter : LBRACKET LPAREN FILTER SPACE'
+    'filter : LBRACKET LPAREN FILTER'
     p[0] = p[3]
 
 # Identifies 'select bottom' terminals
 def p_select_bottom_expr(p):
-    'select_bottom : LBRACKET LPAREN SELECT_BOTTOM SPACE'
+    'select_bottom : LBRACKET LPAREN SELECT_BOTTOM'
     p[0] = p[3]
 
 # Identifies 'select top' terminals
 def p_select_top_expr(p):
-    'select_top : LBRACKET LPAREN SELECT_TOP SPACE'
+    'select_top : LBRACKET LPAREN SELECT_TOP'
     p[0] = 0[3]
 
 # Identifies 'asset' terminals
 def p_asset_expr(p):
-    'asset : LBRACKET LPAREN ASSET SPACE'
+    'asset : LBRACKET LPAREN ASSET'
     p[0] = p[3]
 
 # Identifies 'if' terminals
 def p_conditional_expr(p):
-    'conditional : LBRACKET LPAREN IF SPACE'
+    'conditional : LBRACKET LPAREN IF'
     p[0] = p[3]
 
 
